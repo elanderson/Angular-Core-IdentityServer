8d6d138b2717e9937d3ac37ff2b825aadbb314d2
Eric Anderson <elanderson@gmail.com>
2017-06-12 07:00:32 -0500

    Client app changes for login and logout.

--- IdentityApp/Startup.cs
+++ IdentityApp/Startup.cs
@@ -16,6 +16,8 @@ namespace IdentityApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer()
                 .AddTemporarySigningCredential()
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
