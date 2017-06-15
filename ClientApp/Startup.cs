854a93fd97255330595bfb5de502755ac7c9a45f
Eric Anderson <elanderson@gmail.com>
2017-06-15 13:07:08 -0500

    First round of changes for hybrid flow

--- ClientApp/Startup.cs
+++ ClientApp/Startup.cs
@@ -67,6 +67,12 @@ namespace WebApplicationBasic
                 RequireHttpsMetadata = false,
 
                 ClientId = "mvc",
+                ClientSecret = "secret",
+
+                ResponseType = "code id_token",
+                Scope = { "api1", "offline_access" },
+
+                GetClaimsFromUserInfoEndpoint = true,
                 SaveTokens = true
             });
 
